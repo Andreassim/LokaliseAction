7b2bd9661393d7b179f11adfa576e86e5068d7f8
Andreas Simmelkiaer <andreas.sim@live.dk>
2024-01-11 20:59:15 +0100

    initial try

---
+++ index.js
@@ -0,0 +1,22 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  const API_KEY = core.getInput('API_KEY');
+  const PROJECT_KEY = core.getInput('PROJECT_KEY');
+
+  const lokaliseApi = new LokaliseApi({ apiKey: API_KEY});
+  const downloads = await lokaliseApi.files().download(
+    PROJECT_KEY, 
+    {
+        format: "xlf",
+        original_filenames: false,
+        bundle_structure: "frontend/src/i18n/messages.%LANG_ISO%.%FORMAT%",
+        triggers: ['github']
+    }
+    )
+  core.setOutput("result", "success");
+  
+} catch (error) {
+  core.setFailed(error.message);
+}
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Lokalize Action
+
+This actions is for triggering a "build" in lokalise using their node API.
+
+## Inputs
+
+### `API_KEY`
+
+**Required** API_Key for lokalise.
+
+### `PROJECT_KEY`
+
+**Required** The project key for the lokalise project.
