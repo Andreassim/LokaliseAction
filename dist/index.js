ce22ab4d2d5b726f533405b6c2eebe7c42da3b12
Andreas Simmelkiaer <andreas.sim@live.dk>
2024-01-13 11:50:07 +0100

    v1.8

--- dist/index.js
+++ dist/index.js
@@ -26406,58 +26406,52 @@ function parseParams (str) {
 module.exports = parseParams
 
 
-/***/ }),
+/***/ })
 
-/***/ 2171:
-/***/ ((__webpack_module__, __unused_webpack___webpack_exports__, __nccwpck_require__) => {
+/******/ });
+/************************************************************************/
+/******/ // The module cache
+/******/ var __webpack_module_cache__ = {};
+/******/ 
+/******/ // The require function
+/******/ function __nccwpck_require__(moduleId) {
+/******/ 	// Check if module is in cache
+/******/ 	var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 	if (cachedModule !== undefined) {
+/******/ 		return cachedModule.exports;
+/******/ 	}
+/******/ 	// Create a new module (and put it into the cache)
+/******/ 	var module = __webpack_module_cache__[moduleId] = {
+/******/ 		// no module.id needed
+/******/ 		// no module.loaded needed
+/******/ 		exports: {}
+/******/ 	};
+/******/ 
+/******/ 	// Execute the module function
+/******/ 	var threw = true;
+/******/ 	try {
+/******/ 		__webpack_modules__[moduleId].call(module.exports, module, module.exports, __nccwpck_require__);
+/******/ 		threw = false;
+/******/ 	} finally {
+/******/ 		if(threw) delete __webpack_module_cache__[moduleId];
+/******/ 	}
+/******/ 
+/******/ 	// Return the exports of the module
+/******/ 	return module.exports;
+/******/ }
+/******/ 
+/************************************************************************/
+/******/ /* webpack/runtime/compat */
+/******/ 
+/******/ if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = new URL('.', import.meta.url).pathname.slice(import.meta.url.match(/^file:\/\/\/\w:/) ? 1 : 0, -1) + "/";
+/******/ 
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
 
-__nccwpck_require__.a(__webpack_module__, async (__webpack_handle_async_dependencies__, __webpack_async_result__) => { try {
-/* harmony import */ var _actions_core__WEBPACK_IMPORTED_MODULE_0__ = __nccwpck_require__(6150);
-/* harmony import */ var _lokalise_node_api__WEBPACK_IMPORTED_MODULE_1__ = __nccwpck_require__(9327);
-
-
-
-try {
-    console.log("Getting keys");
-    const API_KEY = _actions_core__WEBPACK_IMPORTED_MODULE_0__.core.getInput('API_KEY');
-    const PROJECT_KEY = _actions_core__WEBPACK_IMPORTED_MODULE_0__.core.getInput('PROJECT_KEY');
-    
-    console.log("Setting up lokaliseAPI");
-    const lokaliseApi = new _lokalise_node_api__WEBPACK_IMPORTED_MODULE_1__/* .LokaliseApi */ .ej({ apiKey: API_KEY});
-    console.log("Download Translations");
-    const downloads = await lokaliseApi.files().download(
-        PROJECT_KEY, 
-        {
-            format: "xlf",
-            original_filenames: false,
-            bundle_structure: "frontend/src/i18n/messages.%LANG_ISO%.%FORMAT%",
-            triggers: ['github']
-        }
-    );
-    
-    console.log("success");
-    _actions_core__WEBPACK_IMPORTED_MODULE_0__.core.setOutput("result", "success");
-        
-} catch (error) {
-    _actions_core__WEBPACK_IMPORTED_MODULE_0__.core.setFailed(error.message);
-}
-
-__webpack_async_result__();
-} catch(e) { __webpack_async_result__(e); } }, 1);
-
-/***/ }),
-
-/***/ 9327:
-/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __nccwpck_require__) => {
-
-
-// EXPORTS
-__nccwpck_require__.d(__webpack_exports__, {
-  "ej": () => (/* reexport */ lokalise_api_LokaliseApi)
-});
-
-// UNUSED EXPORTS: LokaliseApiOAuth, LokaliseApiOta, LokaliseAuth, LokaliseOtaBundles
-
+// EXTERNAL MODULE: ./node_modules/@actions/core/lib/core.js
+var core = __nccwpck_require__(6150);
 ;// CONCATENATED MODULE: ./node_modules/@lokalise/node-api/dist/lokalise/base_client.js
 class base_client_BaseClient {
     clientData = {
@@ -27906,137 +27900,42 @@ class LokaliseAuth {
 
 
 //# sourceMappingURL=main.js.map
+;// CONCATENATED MODULE: ./main.js
 
-/***/ })
 
-/******/ });
-/************************************************************************/
-/******/ // The module cache
-/******/ var __webpack_module_cache__ = {};
-/******/ 
-/******/ // The require function
-/******/ function __nccwpck_require__(moduleId) {
-/******/ 	// Check if module is in cache
-/******/ 	var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 	if (cachedModule !== undefined) {
-/******/ 		return cachedModule.exports;
-/******/ 	}
-/******/ 	// Create a new module (and put it into the cache)
-/******/ 	var module = __webpack_module_cache__[moduleId] = {
-/******/ 		// no module.id needed
-/******/ 		// no module.loaded needed
-/******/ 		exports: {}
-/******/ 	};
-/******/ 
-/******/ 	// Execute the module function
-/******/ 	var threw = true;
-/******/ 	try {
-/******/ 		__webpack_modules__[moduleId].call(module.exports, module, module.exports, __nccwpck_require__);
-/******/ 		threw = false;
-/******/ 	} finally {
-/******/ 		if(threw) delete __webpack_module_cache__[moduleId];
-/******/ 	}
-/******/ 
-/******/ 	// Return the exports of the module
-/******/ 	return module.exports;
-/******/ }
-/******/ 
-/************************************************************************/
-/******/ /* webpack/runtime/async module */
-/******/ (() => {
-/******/ 	var webpackQueues = typeof Symbol === "function" ? Symbol("webpack queues") : "__webpack_queues__";
-/******/ 	var webpackExports = typeof Symbol === "function" ? Symbol("webpack exports") : "__webpack_exports__";
-/******/ 	var webpackError = typeof Symbol === "function" ? Symbol("webpack error") : "__webpack_error__";
-/******/ 	var resolveQueue = (queue) => {
-/******/ 		if(queue && !queue.d) {
-/******/ 			queue.d = 1;
-/******/ 			queue.forEach((fn) => (fn.r--));
-/******/ 			queue.forEach((fn) => (fn.r-- ? fn.r++ : fn()));
-/******/ 		}
-/******/ 	}
-/******/ 	var wrapDeps = (deps) => (deps.map((dep) => {
-/******/ 		if(dep !== null && typeof dep === "object") {
-/******/ 			if(dep[webpackQueues]) return dep;
-/******/ 			if(dep.then) {
-/******/ 				var queue = [];
-/******/ 				queue.d = 0;
-/******/ 				dep.then((r) => {
-/******/ 					obj[webpackExports] = r;
-/******/ 					resolveQueue(queue);
-/******/ 				}, (e) => {
-/******/ 					obj[webpackError] = e;
-/******/ 					resolveQueue(queue);
-/******/ 				});
-/******/ 				var obj = {};
-/******/ 				obj[webpackQueues] = (fn) => (fn(queue));
-/******/ 				return obj;
-/******/ 			}
-/******/ 		}
-/******/ 		var ret = {};
-/******/ 		ret[webpackQueues] = x => {};
-/******/ 		ret[webpackExports] = dep;
-/******/ 		return ret;
-/******/ 	}));
-/******/ 	__nccwpck_require__.a = (module, body, hasAwait) => {
-/******/ 		var queue;
-/******/ 		hasAwait && ((queue = []).d = 1);
-/******/ 		var depQueues = new Set();
-/******/ 		var exports = module.exports;
-/******/ 		var currentDeps;
-/******/ 		var outerResolve;
-/******/ 		var reject;
-/******/ 		var promise = new Promise((resolve, rej) => {
-/******/ 			reject = rej;
-/******/ 			outerResolve = resolve;
-/******/ 		});
-/******/ 		promise[webpackExports] = exports;
-/******/ 		promise[webpackQueues] = (fn) => (queue && fn(queue), depQueues.forEach(fn), promise["catch"](x => {}));
-/******/ 		module.exports = promise;
-/******/ 		body((deps) => {
-/******/ 			currentDeps = wrapDeps(deps);
-/******/ 			var fn;
-/******/ 			var getResult = () => (currentDeps.map((d) => {
-/******/ 				if(d[webpackError]) throw d[webpackError];
-/******/ 				return d[webpackExports];
-/******/ 			}))
-/******/ 			var promise = new Promise((resolve) => {
-/******/ 				fn = () => (resolve(getResult));
-/******/ 				fn.r = 0;
-/******/ 				var fnQueue = (q) => (q !== queue && !depQueues.has(q) && (depQueues.add(q), q && !q.d && (fn.r++, q.push(fn))));
-/******/ 				currentDeps.map((dep) => (dep[webpackQueues](fnQueue)));
-/******/ 			});
-/******/ 			return fn.r ? promise : getResult();
-/******/ 		}, (err) => ((err ? reject(promise[webpackError] = err) : outerResolve(exports)), resolveQueue(queue)));
-/******/ 		queue && (queue.d = 0);
-/******/ 	};
-/******/ })();
-/******/ 
-/******/ /* webpack/runtime/define property getters */
-/******/ (() => {
-/******/ 	// define getter functions for harmony exports
-/******/ 	__nccwpck_require__.d = (exports, definition) => {
-/******/ 		for(var key in definition) {
-/******/ 			if(__nccwpck_require__.o(definition, key) && !__nccwpck_require__.o(exports, key)) {
-/******/ 				Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 			}
-/******/ 		}
-/******/ 	};
-/******/ })();
-/******/ 
-/******/ /* webpack/runtime/hasOwnProperty shorthand */
-/******/ (() => {
-/******/ 	__nccwpck_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
-/******/ })();
-/******/ 
-/******/ /* webpack/runtime/compat */
-/******/ 
-/******/ if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = new URL('.', import.meta.url).pathname.slice(import.meta.url.match(/^file:\/\/\/\w:/) ? 1 : 0, -1) + "/";
-/******/ 
-/************************************************************************/
-/******/ 
-/******/ // startup
-/******/ // Load entry module and return exports
-/******/ // This entry module used 'module' so it can't be inlined
-/******/ var __webpack_exports__ = __nccwpck_require__(2171);
-/******/ __webpack_exports__ = await __webpack_exports__;
-/******/ 
+
+
+async function run(){
+    try {
+        console.log("Getting keys");
+        const API_KEY = core.core.getInput('API_KEY');
+        const PROJECT_KEY = core.core.getInput('PROJECT_KEY');
+        
+        console.log("Setting up lokaliseAPI");
+        const lokaliseApi = new lokalise_api_LokaliseApi({ apiKey: API_KEY});
+        console.log("Download Translations");
+        const downloads = await lokaliseApi.files().download(
+            PROJECT_KEY, 
+            {
+                format: "xlf",
+                original_filenames: false,
+                bundle_structure: "frontend/src/i18n/messages.%LANG_ISO%.%FORMAT%",
+                triggers: ['github']
+            }
+        );
+        
+        console.log("success");
+        core.core.setOutput("result", "success");
+            
+    } catch (error) {
+        core.core.setFailed(error.message);
+    }
+}
+
+
+;// CONCATENATED MODULE: ./index.js
+
+
+run();
+})();
+
--- index.js
+++ index.js
@@ -1,27 +1,3 @@
-import { core } from "@actions/core"
-import { LokaliseApi } from "@lokalise/node-api";
+import { run } from "./main.js"
 
-try {
-    console.log("Getting keys");
-    const API_KEY = core.getInput('API_KEY');
-    const PROJECT_KEY = core.getInput('PROJECT_KEY');
-    
-    console.log("Setting up lokaliseAPI");
-    const lokaliseApi = new LokaliseApi({ apiKey: API_KEY});
-    console.log("Download Translations");
-    const downloads = await lokaliseApi.files().download(
-        PROJECT_KEY, 
-        {
-            format: "xlf",
-            original_filenames: false,
-            bundle_structure: "frontend/src/i18n/messages.%LANG_ISO%.%FORMAT%",
-            triggers: ['github']
-        }
-    );
-    
-    console.log("success");
-    core.setOutput("result", "success");
-        
-} catch (error) {
-    core.setFailed(error.message);
-}
+run();
