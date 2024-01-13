7c6dfa9e2aa1492c018cb1b6f766f45e2f847d5d
Andreas Simmelkiaer <andreas.sim@live.dk>
2024-01-13 10:03:53 +0100

    v1.3

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
 import { core } from "@actions/core"
-import { github } from "@actions/github"
 import { LokaliseApi } from "@lokalise/node-api";
 
-try {
-  const API_KEY = core.getInput('API_KEY');
-  const PROJECT_KEY = core.getInput('PROJECT_KEY');
 
-  const lokaliseApi = new LokaliseApi({ apiKey: API_KEY});
-  const downloads = await lokaliseApi.files().download(
-    PROJECT_KEY, 
-    {
-        format: "xlf",
-        original_filenames: false,
-        bundle_structure: "frontend/src/i18n/messages.%LANG_ISO%.%FORMAT%",
-        triggers: ['github']
+async function run(){
+    try {
+        console.log("Getting keys");
+        const API_KEY = core.getInput('API_KEY');
+        const PROJECT_KEY = core.getInput('PROJECT_KEY');
+        
+        console.log("Setting up lokaliseAPI");
+        const lokaliseApi = new LokaliseApi({ apiKey: API_KEY});
+        console.log("Download Translations");
+        const downloads = await lokaliseApi.files().download(
+            PROJECT_KEY, 
+            {
+                format: "xlf",
+                original_filenames: false,
+                bundle_structure: "frontend/src/i18n/messages.%LANG_ISO%.%FORMAT%",
+                triggers: ['github']
+            }
+        );
+        
+        console.log("success");
+        core.setOutput("result", "success");
+            
+    } catch (error) {
+        core.setFailed(error.message);
     }
-    )
-  core.setOutput("result", "success");
-
-} catch (error) {
-  core.setFailed(error.message);
 }
+
+run()
+
